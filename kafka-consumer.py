a1cc5da569e37510fe0b5528fd7e046977ac81a6
recepcavusoglu <recepcvsgl26@gmail.com>
2021-03-14 15:32:22 +0300

    minor fixes and subreddits added.

--- kafka-consumer.py
+++ kafka-consumer.py
@@ -2,16 +2,20 @@ from kafka import KafkaConsumer
 import json
 import pymongo
 
-consumer= KafkaConsumer('test',bootstrap_servers=['localhost:9092'],api_version=(0,10))
-print("Consumer started...")
-
-myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-mydb = myclient["reddit"]
-
-
-for message in consumer:
-    msg=json.loads(message.value)
-    sub=msg['sub']
+def write_data(sub,msg):
+    myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+    mydb = myclient["reddit"]
     mycol=mydb[sub]
-    msg.pop('sub')
     x = mycol.insert_one(msg)
+
+def consumer():
+    print("Consumer started...")
+    consumer= KafkaConsumer('test',bootstrap_servers=['localhost:9092'],api_version=(0,10))
+    for message in consumer:
+        msg=json.loads(message.value)
+        sub=msg['sub']        
+        msg.pop('sub')
+        write_data(sub,msg)
+    
+if __name__=="__main__":
+    consumer()
--- multiprocess.py
+++ multiprocess.py
@@ -3,6 +3,11 @@ import json
 import concurrent.futures
 from kafka import KafkaProducer
 
+def get_subs(json_file="subreddits.json"):    
+    with open(json_file) as f:
+        subs= json.load(f)
+    return subs['subreddits'],subs['postcounts']
+
 def create_reddit_object(json_file="reddit_config.json"):
     with open(json_file) as f:
         user_values= json.load(f)
@@ -13,21 +18,22 @@ def create_reddit_object(json_file="reddit_config.json"):
     return reddit
 
 
-def Producer(p_message,p_topicname):
+def producer(p_message,p_topicname):
     producer= KafkaProducer(bootstrap_servers=['localhost:9092'],api_version=(0,10,1)) #add value_serializer parameter 
     message=json.dumps(p_message,ensure_ascii=False).encode('utf-8')
     producer.send(p_topicname,message)
 
-def getData(sub,count=50):
+def get_sub_data(sub,post_count):
     reddit= create_reddit_object()
     print(f"{sub} data Collecting...")
     subred=reddit.subreddit(sub)
-    new= subred.new(limit=count)    
+    new= subred.new(limit=post_count)    
     for i in new:
-        Producer({"sub":sub,"title":i.title,"author":str(i.author),"shortlink":i.shortlink},'test')
+        #producer({"sub":sub,"title":i.title,"author":str(i.author),"shortlink":i.shortlink},'test')
+        pass
 
 
 if __name__=="__main__":
     with concurrent.futures.ProcessPoolExecutor() as executor:
-        subreds=["turkey","learnprogramming","ankara"]
-        results=executor.map(getData,subreds)
+        subreds,postcounts=get_subs()    
+        results=executor.map(get_sub_data,subreds,postcounts)
