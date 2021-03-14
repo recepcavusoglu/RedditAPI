46cbefc47de0eb8f657f7ceb736a90d19a9c45cf
recepcavusoglu <recepcvsgl26@gmail.com>
2021-03-14 22:15:17 +0300

    create_table added.

--- kafka-consumer.py
+++ kafka-consumer.py
@@ -4,27 +4,30 @@ import pymongo
 
 def connect_database():#get connection from config file
     myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-    return myclient
+    database= myclient["reddit"]
+    return database
 
-def create_table(tablename):#table create
-    pass
+def create_table(tablename,msg):
+    database= connect_database()
+    mycol=database[tablename]
+    x=mycol.insert_one(msg)
+    print(tablename," crated")
 
 def write_data(sub,msg):
-    myclient= connect_database()
-    database = myclient["reddit"]
-    mycol=database[sub]
-    x = mycol.insert_one(msg)
-    print("instered")
-    myclient.close()
+    database= connect_database()
+    colnames=database.list_collection_names()
+    if sub in colnames:
+        mycol=database[sub]
+        x = mycol.insert_one(msg)
+        print(sub," inserted")
+    else:
+        create_table(sub,msg)
 
 def consumer():
     print("Consumer started...")
     consumer= KafkaConsumer('test',bootstrap_servers=['localhost:9092'],api_version=(0,10))
-    i=0
     for message in consumer:
         msg=json.loads(message.value)
-        print(i)
-        i+=1
         sub=msg['sub']        
         msg.pop('sub')
         write_data(sub,msg)
