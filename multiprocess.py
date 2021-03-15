f80b83eb2a3506ab92335b7783217a3b6763ed59
recepcavusoglu <recepcvsgl26@gmail.com>
2021-03-16 00:19:15 +0300

    redis check added.

--- multiprocess.py
+++ multiprocess.py
@@ -6,9 +6,15 @@ from kafka import KafkaProducer
 import schedule
 import sys
 import redis
-#import datetime
+import datetime
+from datetime import timezone
+import time
 
 #sen data and check from redis
+def get_unix_time():
+    dt = datetime.datetime.now()   
+    timestamp = dt.timestamp() 
+    return int(timestamp)
 
 def create_redis(path="config/redis_config.json"):
     with open(path) as f:
@@ -16,6 +22,17 @@ def create_redis(path="config/redis_config.json"):
     client=redis.Redis(host=config['host'],port=config['port'])
     return client
 
+def check_redis(subname):
+    conn=create_redis()
+    return conn.exists(subname)
+
+def get_redis_data(subname):
+    conn=create_redis()
+    return conn.get(subname)
+
+def set_redis_Data(subname,data):
+    conn=create_redis()
+    conn.set(subname,data)
 
 def get_subs(path="config/subreddits.json"):    
     with open(path) as f:
@@ -47,21 +64,31 @@ def producer(p_message,p_topicname):
     except Exception as e:
         print("Producer Error: ",e)
 
-def get_sub_data(sub,post_count=1500):
+def get_sub_data(sub,post_count=5):
     try:
         reddit= create_reddit_object()
         print(f"{sub} data Collecting...")
-        #pull redis data
-        #if redisdata empty:
-        new=reddit.subreddit(sub).new(limit=post_count)
-        #else:
-        #new=reddit.subreddit(sub).new(unixtimestamp start-finish period)
+        if check_redis(sub):
+            print("Redis data founded for ", sub)
+            stop= int(get_redis_data(sub))
+        new=reddit.subreddit(sub).new(limit=post_count)            
     except Exception as e:
         print("API Error: ",e)
         return None
 
+    print("stop: ",stop)
+    start_flag=0
     for i in new:
-        producer({"sub":sub,"title":i.title,"author":str(i.author),"shortlink":i.shortlink},'test')
+        if start_flag==0:
+            set_redis_Data(sub,int(i.created_utc))
+            start_flag+=1
+            print("Redis data updated for: ", sub)
+        
+        if i.created_utc<=stop:
+            print("Data exist stopping")
+            break
+        print({"sub":sub,"title":i.title,"author":str(i.author),"shortlink":i.shortlink})
+        #producer({"sub":sub,"title":i.title,"author":str(i.author),"shortlink":i.shortlink},'test')
 
 def call_data(p_sublist):
     if p_sublist:
@@ -87,11 +114,12 @@ def arg_parser():
         sys.exit(0)
 
 if __name__=="__main__":
-    sublist=False
+    sublist=True
     #sublist=arg_parser()
     call_data(sublist)
     schedule.every(1).minutes.do(call_data,sublist)
     while True:
         schedule.run_pending()
+
 
     
