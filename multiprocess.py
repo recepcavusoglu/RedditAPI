bfa71639e0a8bc97c0c02eb5d7ad7b01dc7a29d1
recepcavusoglu <recepcvsgl26@gmail.com>
2021-03-16 21:30:54 +0300

    commenting.

--- multiprocess.py
+++ multiprocess.py
@@ -9,38 +9,37 @@ import redis
 import datetime
 from datetime import timezone
 import time
-
-
+#creating redis object
 def create_redis(path="config/redis_config.json"):
     with open(path) as f:
         config=json.load(f)
     client=redis.Redis(host=config['host'],port=config['port'])
     return client
-
+#check if redis data exist
 def check_redis(subname):
     conn=create_redis()
     return conn.exists(subname)
-
+#getting redis data
 def get_redis_data(subname):
     conn=create_redis()
     return int(conn.get(subname))
-
+#stting redis data
 def set_redis_Data(subname,data):
     conn=create_redis()
     conn.set(subname,data)
-
+#getting subreddit list via json file
 def get_subs(path="config/subreddits.json"):    
     with open(path) as f:
         subs= json.load(f)
     return subs['subreddits']
-
+#getting subreddit list via user follow
 def get_user_subs():
     reddit = create_reddit_object()
     subs=list(reddit.user.subreddits(limit=None))
     for i in range(len(subs)):
         subs[i]=subs[i].display_name
     return subs
-
+#creating reddit object
 def create_reddit_object(path="config/reddit_config.json"):
     with open(path) as f:
         user_values= json.load(f)
@@ -49,8 +48,7 @@ def create_reddit_object(path="config/reddit_config.json"):
                    user_agent=user_values['user_agent'],
                    username=user_values['username'],password=user_values['password'])
     return reddit
-
-
+#kafka producer
 def producer(p_message,p_topicname):
     try:
         producer= KafkaProducer(bootstrap_servers=['localhost:9092'],api_version=(0,10,1)) #add value_serializer parameter 
@@ -58,8 +56,8 @@ def producer(p_message,p_topicname):
         producer.send(p_topicname,message)
     except Exception as e:
         print("Producer Error: ",e)
-
-def get_sub_data(sub,post_count=100):
+#getting sub data and sending it to producer
+def get_sub_data(sub,post_count=5):
     try:
         stop=0
         reddit= create_reddit_object()
@@ -73,10 +71,9 @@ def get_sub_data(sub,post_count=100):
     except Exception as e:
         print("API Error: ",e)
         return None
-
     start_flag=0
     for i in new:
-        if start_flag==0 and stop!=get_redis_data(sub):
+        if start_flag==0:
             set_redis_Data(sub,int(i.created_utc))
             start_flag+=1
             print("Redis data updated for: ", sub)
@@ -87,7 +84,7 @@ def get_sub_data(sub,post_count=100):
         else:
             print(sub, "Data exist stopping")
             break
-
+#getting subreddit datas and calling proccess
 def call_data(p_sublist):
     if p_sublist:
         subreds=get_subs()
@@ -96,7 +93,7 @@ def call_data(p_sublist):
 
     with concurrent.futures.ProcessPoolExecutor() as executor:          
         results=executor.map(get_sub_data,subreds)
-
+#argparser json is for reading subreddits on json and follow for user follows
 def arg_parser():
     parser=argparse.ArgumentParser()
     parser.add_argument("sublist",help="Please specify subreddit list using either json or follow")
@@ -110,12 +107,12 @@ def arg_parser():
     else:
         print("Wrong usage plese use either [json] or [follow] paramater.")
         sys.exit(0)
-
+#main block runs itself evey minute
 if __name__=="__main__":
-    sublist=False
-    #sublist=arg_parser()
+    sublist=True
+    sublist=arg_parser()
     call_data(sublist)
-    schedule.every(5).minutes.do(call_data,sublist)
+    schedule.every(1).minutes.do(call_data,sublist)
     while True:
         schedule.run_pending()
 
