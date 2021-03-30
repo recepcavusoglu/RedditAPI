680370d38cef42fb7ac676d462c1d2c529d6a092
Recep Çavuşoğlu <33806246+recepcavusoglu@users.noreply.github.com>
2021-03-30 23:15:57 +0300

    producer config added

--- multiprocess.py
+++ multiprocess.py
@@ -9,29 +9,43 @@ import redis
 import datetime
 from datetime import timezone
 import time
+
+#creating kafka object
+def create_producer(path="config/kafka_config.json"):
+    with open(path) as f:
+        config=json.load(f)
+    producer= KafkaProducer(bootstrap_servers=[config['host']],api_version=(0,10,1)) #add value_serializer parameter 
+    topic_name=config['topic_name']
+    return producer
+    
 #creating redis object
 def create_redis(path="config/redis_config.json"):
     with open(path) as f:
         config=json.load(f)
     client=redis.Redis(host=config['host'],port=config['port'])
     return client
+
 #check if redis data exist
 def check_redis(subname):
     conn=create_redis()
     return conn.exists(subname)
+
 #getting redis data
 def get_redis_data(subname):
     conn=create_redis()
     return int(conn.get(subname))
+
 #stting redis data
 def set_redis_Data(subname,data):
     conn=create_redis()
     conn.set(subname,data)
+
 #getting subreddit list via json file
 def get_subs(path="config/subreddits.json"):    
     with open(path) as f:
         subs= json.load(f)
     return subs['subreddits']
+
 #getting subreddit list via user follow
 def get_user_subs():
     reddit = create_reddit_object()
@@ -39,6 +53,7 @@ def get_user_subs():
     for i in range(len(subs)):
         subs[i]=subs[i].display_name
     return subs
+
 #creating reddit object
 def create_reddit_object(path="config/reddit_config.json"):
     with open(path) as f:
@@ -48,29 +63,36 @@ def create_reddit_object(path="config/reddit_config.json"):
                    user_agent=user_values['user_agent'],
                    username=user_values['username'],password=user_values['password'])
     return reddit
+
 #kafka producer
 def producer(p_message,p_topicname):
     try:
-        producer= KafkaProducer(bootstrap_servers=['localhost:9092'],api_version=(0,10,1)) #add value_serializer parameter 
+        producer = create_producer()
         message=json.dumps(p_message,ensure_ascii=False).encode('utf-8')
         producer.send(p_topicname,message)
     except Exception as e:
         print("Producer Error: ",e)
-#getting sub data and sending it to producer
-def get_sub_data(sub,post_count=5):
+
+#getting sub data
+def get_sub_data(sub,post_count=10):
     try:
-        stop=0
+        stop_flag=0
         reddit= create_reddit_object()
         print(f"{sub} data Collecting...")
         if check_redis(sub):
             print("Redis data founded for ", sub)
-            stop= get_redis_data(sub)
+            stop_flag= get_redis_data(sub)
         else:
             print("Redis data not founded for ", sub)
-        new=reddit.subreddit(sub).new(limit=post_count)            
+        
+        new=reddit.subreddit(sub).new(limit=post_count)
+        send_sub_data(new,sub,stop_flag) 
     except Exception as e:
         print("API Error: ",e)
         return None
+
+#sending sub data to producer
+def send_sub_data(new,sub,stop_flag):
     start_flag=0
     for i in new:
         if start_flag==0:
@@ -78,12 +100,13 @@ def get_sub_data(sub,post_count=5):
             start_flag+=1
             print("Redis data updated for: ", sub)
         
-        if i.created_utc>stop:            
-            #print({"sub":sub,"title":i.title,"author":str(i.author),"shortlink":i.shortlink})
+        if i.created_utc>stop_flag:            
+            print({"sub":sub,"title":i.title,"author":str(i.author),"shortlink":i.shortlink})
             producer({"sub":sub,"title":i.title,"author":str(i.author),"shortlink":i.shortlink},'test')
         else:
             print(sub, "Data exist stopping")
             break
+
 #getting subreddit datas and calling proccess
 def call_data(p_sublist):
     if p_sublist:
@@ -93,6 +116,7 @@ def call_data(p_sublist):
 
     with concurrent.futures.ProcessPoolExecutor() as executor:          
         results=executor.map(get_sub_data,subreds)
+
 #argparser json is for reading subreddits on json and follow for user follows
 def arg_parser():
     parser=argparse.ArgumentParser()
@@ -107,6 +131,7 @@ def arg_parser():
     else:
         print("Wrong usage plese use either [json] or [follow] paramater.")
         sys.exit(0)
+
 #main block runs itself evey minute
 if __name__=="__main__":
     sublist=True
@@ -114,7 +139,4 @@ if __name__=="__main__":
     call_data(sublist)
     schedule.every(1).minutes.do(call_data,sublist)
     while True:
-        schedule.run_pending()
-
-
-    
+        schedule.run_pending()    
